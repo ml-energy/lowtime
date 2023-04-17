f6117a1fbead4ce4edadeee88ba2b93ed587c44c
Yile Gu <yilegu@umich.edu>
2023-04-17 00:40:14 -0500

    Add batch run scripts for grid searching initial guesses

---
+++ examples/batch_initial_guess.py
@@ -0,0 +1,64 @@
+import argparse
+import datetime
+import time
+import multiprocessing
+import subprocess
+from pathlib import Path
+import pandas as pd
+
+def run_task(task: dict):
+    # run the driver script
+    arg_list = []
+    for key, value in task.items():
+        if key == "task_name" or key == "driver_path":
+            continue
+        arg_list.append(f"--{key}")
+        arg_list.append(str(value))
+    subprocess.run(["python", task["driver_path"]] + arg_list)
+    return task["task_name"]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--workload_path", type=str, required=True, help="Path for workload file")
+    parser.add_argument("--data_path", type=str, required=True, help="Path for directory containing instruction profile results")
+    parser.add_argument("--p2p_power", type=float, help="Raw P2P blocking power consumption value to use")
+    parser.add_argument("--unit_time", type=float, help="The unit time for PD algorithm")
+    parser.add_argument("--driver_path", type=str, required=True, help="Path for driver script")
+    parser.add_argument("--num_stages", type=int, default=4, help="Number of stages in the pipeline")
+    args = parser.parse_args()
+    # sanity check p2p profile
+    assert(Path(args.driver_path).exists())
+    # import csv dataframe from workload path
+    df = pd.read_csv(args.workload_path)
+
+    tasks = []
+    # assume merak is the framework
+    framework = "merak"
+    # iterate through each row in the dataframe, create a task list
+    for index, row in df.iterrows():
+        base_args = {}
+        # first locate the inst profile
+        base_name = f"{framework}+{row['model']}+{row['partition_method']}+dp{row['dp']}+pp{row['pp']}+tp{row['tp']}+mbs{row['microbatch_size']}"
+        inst_profile = base_name + ".csv"
+        inst_profile = Path(args.data_path) / inst_profile
+        print(inst_profile)
+        # sanity check
+        assert(inst_profile.exists())
+        # create task args, use the same base for all 3 fit methods
+        base_args["driver_path"] = args.driver_path
+        base_args["inst_profile"] = str(inst_profile)
+        # use default values for interval and unit_time
+        base_args["unit_time"] = args.unit_time
+        base_args["p2p_power"] = args.p2p_power
+        base_args["task_name"] = base_name
+        base_args["num_stages"] = args.num_stages
+        tasks.append(base_args)
+
+    with multiprocessing.Pool() as p:
+        res_itr = p.imap_unordered(run_task, tasks)
+        for i, res in enumerate(res_itr):
+            print(f'Completed {i+1}/{len(tasks)}: {res}', end='\r')
+        print('\n')
+
+if __name__ == "__main__":
+    main()
---
+++ examples/exponential.py
@@ -0,0 +1,90 @@
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+import os
+import datetime
+import time
+import argparse
+import pandas as pd
+from pathlib import Path
+from examples.common import df_to_time_costs_pareto, preprocess_time_costs
+from rene  import Forward, Backward
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--inst_profile", type=str, required=True, help="Path for instruction profile results")
+parser.add_argument("--unit_time", type=float, default=0.001, help="The unit of reduction for each iteration, the smaller the value, the more iterations it takes to converge and the finer graunularity for the Pareto frontier")
+parser.add_argument("--p2p_power", type=float, help="Raw P2P blocking power consumption value to use")
+parser.add_argument("--num_stages", type=int, default=4, help="Number of stages in the pipeline")
+args = parser.parse_args()
+
+inst_df = pd.read_csv(args.inst_profile)
+total_time_costs = df_to_time_costs_pareto(inst_df)
+total_time_costs = preprocess_time_costs(total_time_costs, args.unit_time)
+print(total_time_costs)
+   
+p2p_power = args.p2p_power
+
+time_stamp = datetime.datetime.fromtimestamp(
+        time.time()).strftime('%m%d_%H%M%S')
+profile_path = Path(args.inst_profile)
+output_dir = Path("/users/yilegu/exponential") / profile_path.name / time_stamp
+# output_dir = Path(output_dir)
+output_dir.mkdir(parents=True, exist_ok=False)
+
+cands_a = [1e+1, 1e+2, 1e+3]
+cands_b = np.array([-0.01, -0.02, -0.03, -0.04, -0.05, -0.1])
+cands_b /= (args.unit_time / 0.001)  
+cands_b = list(cands_b)
+cands_c = [1e+1, 1e+2, 1e+3]
+
+for inst_type in [Forward, Backward]:
+    for stage_id in range(args.num_stages):
+        time_costs = total_time_costs[inst_type][stage_id]
+        time_costs.sort(key=lambda x: x[0], reverse=True)
+        time_list = []
+        cost_list = []
+        freq_list = []
+        cost_list_unrefined = []
+        for t, e, f in time_costs:
+            time_list.append(t)
+            cost_list_unrefined.append(e)
+            cost_list.append(e - p2p_power * t * args.unit_time)
+            freq_list.append(f)
+        for i, cand_a in enumerate(cands_a):
+            for j, cand_b in enumerate(cands_b):
+                for k, cand_c in enumerate(cands_c):
+                    p0 = [cand_a, cand_b, cand_c]
+                    fit_coeffs, _ = curve_fit(
+                        lambda t, a, b, c: a * np.exp(b * t) + c,
+                        time_list,
+                        cost_list,
+                        p0=p0,
+                        maxfev=10000,
+                    ) 
+                    fig, ax = plt.subplots(figsize=(8, 8), tight_layout=True)
+                    ax.plot(time_list, cost_list, "o")
+                    for i in range(len(time_list)):
+                        ax.annotate(
+                            f"({time_list[i]:.6f}, {cost_list[i]:.6f}, {freq_list[i]})",
+                            (time_list[i], cost_list[i]),
+                        )
+                    # ax.plot(time_list, cost_list_unrefined, "x")
+                    # generate a list with step size 0.1
+                    x = np.arange(min(time_list), max(time_list), 0.0001)
+                    # ax.plot(x, np.polyval(self.fit_coeffs, x), 'r-')
+                    y = []
+                    for m in x:
+                        y.append(fit_coeffs[0] * np.exp(fit_coeffs[1] * m) + fit_coeffs[2])
+                    # a, b, c = unrefined_fit_coeffs
+                    # unrefined_y = a * np.exp(b * x) + 
+                    # refined_y = []
+                    # for i in x:
+                    #     refined_y.append(self.get_p2p_refined_cost(i))
+                    ax.plot(x, y, "r-")
+                    ax.set_xlabel("time")
+                    ax.set_ylabel("energy")
+                    fig.savefig(
+                        os.path.join(str(output_dir), f"exopnential_{inst_type}_{stage_id}_{p0}.png"), format="PNG"
+                    )
+                    plt.clf()
+                    plt.close()
