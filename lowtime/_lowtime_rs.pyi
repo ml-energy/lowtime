92656ff628fd581be6e36ad5747f5b34fd7f55e8
Oh Jun Kweon <ohjunjps@gmail.com>
2024-11-07 14:28:55 -0500

    fix + reformat pyi

--- lowtime/_lowtime_rs.pyi
+++ lowtime/_lowtime_rs.pyi
@@ -3,8 +3,6 @@ from __future__ import annotations
 import networkx as nx
 
 class PhillipsDessouky:
-    node_ids: list[int]
-
     def __init__(
         self,
         node_ids: list[int] | nx.classes.reportviews.NodeView,
@@ -12,4 +10,5 @@ class PhillipsDessouky:
         sink_node_id: int,
         edges_raw: list[tuple[tuple[int, int], float]],
     ) -> None: ...
+
     def max_flow(self) -> list[tuple[tuple[int, int], float]]: ...
