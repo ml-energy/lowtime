e4aff752994db4002073dfbd8d550f2c95a09104
Oh Jun Kweon <ohjunjps@gmail.com>
2024-11-07 12:48:15 -0500

    fix type hint

--- lowtime/_lowtime_rs.pyi
+++ lowtime/_lowtime_rs.pyi
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+import networkx as nx
+
 class PhillipsDessouky:
     node_ids: list[int]
 
     def __init__(
         self,
-        node_ids: list[int],
+        node_ids: list[int] | nx.classes.reportviews.NodeView,
         source_node_id: int,
         sink_node_id: int,
         edges_raw: list[tuple[tuple[int, int], float]],
