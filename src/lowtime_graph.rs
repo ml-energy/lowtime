293579d738d5f2dba45ab7404cac02e3633591a5
Oh Jun Kweon <ohjunjps@gmail.com>
2024-11-11 11:59:21 -0500

    graph updates

--- src/lowtime_graph.rs
+++ src/lowtime_graph.rs
@@ -1,5 +1,6 @@
 use pyo3::prelude::*;
 
+use std::collections::HashMap;
 use ordered_float::OrderedFloat;
 
 use std::time::Instant;
@@ -8,15 +9,51 @@ use log::info;
 use crate::utils;
 
 
-#[pyclass]
 pub struct LowtimeGraph {
+    edges: HashMap<u32, HashMap<u32, Operation>>,
+    preds: HashMap<u32, u32>,
 }
 
-#[pymethods]
 impl LowtimeGraph {
-    #[new]
-    fn new(
-    ) -> PyResult<Self> {
-        Ok(LowtimeGraph {})
+    pub fn new() -> Self {
+        LowtimeGraph {
+            edges: HashMap::new(),
+            preds: HashMap::new(),
+        }
     }
+
+    pub fn get_nodes(&self) -> Vec<u32> {
+        let mut nodes: Vec<u32> = self.edges.keys().cloned().collect();
+        nodes.sort();
+        nodes
+    }
+
+    pub fn add_edge(&mut self, from: u32, to: u32, op: Operation) -> () {
+        self.edges
+            .entry(from)
+            .or_insert_with(HashMap::new)
+            .insert(to, op);
+        self.preds.insert(to, from);
+    }
+
+    pub fn get_op(&self, from: u32, to: u32) -> Option<&Operation> {
+        self.edges
+            .get(&from)
+            .and_then(|ops| ops.get(&to))
+    }
+
+    pub fn get_mut_op(&mut self, from: u32, to: u32) -> Option<&mut Operation> {
+        self.edges
+            .get_mut(&from)
+            .and_then(|ops| ops.get_mut(&to))
+    }
+
+
+}
+
+struct Operation {
+    capacity: OrderedFloat<f64>,
+    flow: OrderedFloat<f64>,
+    ub: OrderedFloat<f64>,
+    lb: OrderedFloat<f64>,
 }
