b5911fc1422678364e3a19f30d9e7235edebc18b
Oh Jun Kweon <ohjunjps@gmail.com>
2024-11-11 13:42:52 -0500

    floats instead of int

--- src/operation.rs
+++ src/operation.rs
@@ -8,9 +8,9 @@ pub trait CostModel {
 pub struct Operation<C: CostModel> {
     capacity: OrderedFloat<f64>,
     flow: OrderedFloat<f64>,
+    ub: OrderedFloat<f64>,
+    lb: OrderedFloat<f64>,
     // TODO(ohjun): should we have a separate struct for node values above and operation values?
-    ub: u32,
-    lb: u32,
     duration: u32,
     cost_model: C,
 }
@@ -19,12 +19,12 @@ impl<C> Operation<C>
 where
     C: CostModel,
 {
-    fn new(capacity: f64, flow: f64, ub: u32, lb: u32, duration: u32, cost_model: C) -> Self {
+    fn new(capacity: f64, flow: f64, ub: f64, lb: f64, duration: u32, cost_model: C) -> Self {
         Operation {
             capacity: OrderedFloat(capacity),
             flow: OrderedFloat(flow),
-            ub,
-            lb,
+            ub: OrderedFloat(ub),
+            lb: OrderedFloat(lb),
             duration,
             cost_model,
         }
@@ -38,11 +38,11 @@ where
         self.flow
     }
 
-    fn get_ub(&self) -> u32 {
+    fn get_ub(&self) -> OrderedFloat<f64> {
         self.ub
     }
 
-    fn get_lb(&self) -> u32 {
+    fn get_lb(&self) -> OrderedFloat<f64> {
         self.lb
     }
 
